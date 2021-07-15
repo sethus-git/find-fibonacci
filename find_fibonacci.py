8ddbed86f315950aa7091510e54a86779efb36e8
Ransome <Ransome>
2021-07-15 14:13:31 +0000

    modified the script to work with both python2 and python3

--- find_fibonacci.py
+++ find_fibonacci.py
@@ -6,12 +6,14 @@ def find_fibonacci(limit):
     elif limit == 2:
         print(','.join(map(str, lst)))
     else:
-        for i in range(3,limit+1):
+        for i in range(3,int(limit)+1):
             length = len(lst)
             second_last_number = length - 2
             lst.append(lst[-1] + lst[second_last_number])
 
         print(','.join(map(str, lst)))
         
+#limit = input('Enter a non zero integer number : ')
+#find_fibonacci(limit)
 
 find_fibonacci(8)
